d029fa80b4894c6108f8056eb5f82e27a878877d
Darki8 <131977013+Darki8@users.noreply.github.com>
2024-06-19 15:52:50 +0200

    Debugging errors

--- init_db.sh
+++ init_db.sh
@@ -1,33 +1,37 @@
 #!/bin/bash
 set -e
-set -x
 
 # Source the .env file (assuming it's located at /app/.env inside the container)
 source /app/.env
+DB_USER=${MYSQL_USER}
+DB_PASSWORD=${MYSQL_PASSWORD}
+
+mysql -u "${DB_USER}" -p"${DB_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS Test01;"
 
 # Create SQL statements using environment variables
-SQL="
-CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
-USE ${MYSQL_DATABASE};
+#
+#SQL="
+#CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+#USE ${MYSQL_DATABASE};
 
-CREATE TABLE IF NOT EXISTS info (
+#CREATE TABLE IF NOT EXISTS info (
     id INT AUTO_INCREMENT PRIMARY KEY,
     f_name VARCHAR(30) NOT NULL,
     l_name VARCHAR(30) NOT NULL,
     phone_no INT NOT NULL
-);
-CREATE USER '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';
+#);
+#CREATE USER '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';
 
-GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
-FLUSH PRIVILEGES;
-"
+#GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+#FLUSH PRIVILEGES;
+#"
 
 # Output SQL for debugging purposes
-echo "SQL: "
-echo "${SQL}"
+#echo "SQL: "
+#echo "${SQL}"
 
-echo "MYSQL_ROOT_PASSWORD: ${MYSQL_ROOT_PASSWORD}"
+#echo "MYSQL_ROOT_PASSWORD: ${MYSQL_ROOT_PASSWORD}"
 
 
 # Run the SQL statements using mysql client 
-mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "${SQL}"
+#mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "${SQL}"
